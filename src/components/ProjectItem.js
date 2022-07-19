31a487a81812ff7372cfb6666447d49ae65d6f0d
Garret Gallant <gallanttechnological@gmail.com>
2022-07-19 16:42:45 -0600

    lesson complete

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  
+const renderTech = (technologies) => technologies.map((technology) => (
+    <span key={technology.id}> {technology} </span>
+))
+  
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {renderTech(technologies)}
       </div>
     </div>
   );
